8d9a22ed9f23309fa67591ad9e2206673d47a3c7
michael <michaellee4@gmail.com>
2018-12-28 22:11:49 -0600

    added engine singleton

--- include/engine.h
+++ include/engine.h
@@ -1,14 +1,24 @@
 #ifndef ENGINE_H
 #define ENGINE_H
 #include <memory>
+#include "protocol.h"
 
 class Engine
 {
-private:
-	void printGreeting();
 public:
-	Engine();
-	void start();
+	static Engine& getInstance()
+	{
+		static Engine instance;
+		return instance;
+	}
+private:
+	Engine() noexcept;
+	void printGreeting() const noexcept;
+	std::unique_ptr<ProtocolManager> protocol;
+public:
+	Engine(const Engine&) = delete;
+	void operator=(const Engine&) = delete;
+	void start() noexcept;
 };
 
 #endif
--- src/engine.cc
+++ src/engine.cc
@@ -1,7 +1,53 @@
 #include "engine.h"
+#include "defs.h"
+#include "init.h"
+#include "uci.h"
+#include "xboard.h"
+#include "console.h"
 #include <iostream>
-void Engine::printGreeting()
+
+Engine::Engine() noexcept: protocol() 
+{
+	//Only ever called once because engine is a singleton
+	Init::initAll();
+}
+
+void Engine::printGreeting() const noexcept
 {
 	std::cout << "C++ Chess Engine" << std::endl;
 	std::cout << "Type \'console\' for an interactive console\n" << std::endl;
+}
+
+void Engine::start() noexcept
+{
+	this->printGreeting();
+	bool gameOver = false;
+	std::string buf;
+	while(!gameOver)
+	{
+		std::cout << std::flush;
+		if(!(getline (std::cin, buf))) { continue; }
+		if(buf[0] == '\n') { continue; }
+		if(buf == "uci")
+		{
+			this->protocol = std::make_unique<UCIManager>();
+		}
+		else if(buf == "xboard")
+		{
+			this->protocol = std::make_unique<XBoardManager>();
+		}
+		else if(buf == "console")
+		{
+			this->protocol = std::make_unique<ConsoleManager>();
+		}
+		else if(buf == "q" || buf == "quit")
+		{
+			gameOver = true;
+		}
+		if(this->protocol)
+		{
+			this->protocol->loop();
+			gameOver = this->protocol->isOver();
+		}
+	}	
 }
